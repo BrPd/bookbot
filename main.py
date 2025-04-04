dd4aab6981d99ae94a0668d70527024a15463553
BrPd <b4brijesh.pandey@gmail>
2025-04-04 17:54:17 +0530

    Added a func. to read file

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def get_book_txt(filepath):
+    file_contents = None
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    print(get_book_txt(filepath))
+
+
+filepath = "./books/frankenstein.txt"
+main()
+
