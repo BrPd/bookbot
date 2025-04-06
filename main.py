b6256d28702c601e2e3b2b8010f8413e74499e89
BrPd <b4brijesh.pandey@gmail>
2025-04-06 09:13:15 +0530

    Printed a Report

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from stats import count_words
 from stats import count_chars
+from stats import sorted_list_of_dict
 
+# opens the file and reads it's contents into a string
 def get_book_txt(filepath):
     file_contents = None
     with open(filepath) as f:
@@ -8,11 +10,24 @@ def get_book_txt(filepath):
     return file_contents
 
 def main():
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    
     text = get_book_txt(filepath)
     num_words = count_words(text)
     print(f"{num_words} words found in the document")
-    char_count_dict = count_chars(text)
-    print(char_count_dict)
+    
+    print("--------- Character Count -------")
+    char_count_dict = count_chars(text)             # to count book characters
+    list = sorted_list_of_dict(char_count_dict)     # to sort(high => low) characters acc. to their count
+    for i in list:
+        if i['char'].isalpha() == True:             # only print if the character is an alphabet
+            print(f"{i['char']}: {i['count']}")
+
+    print("============= END ===============")
+
+
 
 
 filepath = "./books/frankenstein.txt"
--- stats.py
+++ stats.py
@@ -1,15 +1,46 @@
+# counts no. of words in the book. Accepts a singular string
 def count_words(book_text):
-    text_list = book_text.split()
+    text_list = book_text.split()  # .split() => splits the string on a delimiter
     word_count = 0
     for i in text_list:
         word_count += 1
     return word_count
 
+# counts no. of characters in the book. Accepts a single string 
+#Returns a dictionary with char-count (key-value) pair.
 def count_chars(text):
     char_dict = {}
     for i in text:
-        char = i.lower()
-        if char not in char_dict:
+        char = i.lower()    # we want uppercase characters to be also counted as lowercase to avoid duplicates
+        if char not in char_dict: # set char count = 0 for first timers
             char_dict[char] = 0
         char_dict[char] += 1 
     return char_dict
+
+# A function that takes a dictionary and returns the value of the "num" key
+# This is how the `.sort()` method knows how to sort the list of dictionaries
+def sort_on(dict):
+    return dict["count"]
+
+# Accepts a char: count dictionary. Returns a sorted list of dictionaries, wherein 
+# each dictionary contains 2 key-value pair, 1 for the character and 1 for it's count. 
+# For eg: list = [
+#           {"char": 'e', "count": 44538}, 
+#           {"char": 't', "count": 29493}, 
+#           {"char": 'a', "count": 25894}
+#        ]
+def sorted_list_of_dict(dict):
+    list = []
+    for i in dict:
+        list_dict = {}
+        list_dict["char"] = i
+        list_dict["count"] = dict[i]
+        list.append(list_dict)
+    list.sort(reverse=True, key=sort_on)
+    return list
+
+
+
+
+
+
